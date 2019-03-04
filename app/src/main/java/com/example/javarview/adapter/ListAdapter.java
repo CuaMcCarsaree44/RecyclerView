2ad400c298c5a7ce7b38dcaa9629e62844e2c13e
CuaMcCarsaree44 <yosuakristianto754@gmail.com>
2019-03-04 16:16:32 +0700

    Updating

--- app/src/main/java/com/example/javarview/adapter/ListAdapter.java
+++ app/src/main/java/com/example/javarview/adapter/ListAdapter.java
@@ -2,54 +2,70 @@ package com.example.javarview.adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import com.example.javarview.R;
+import com.example.javarview.model.PLanguage;
+import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
+import java.util.Vector;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.Handler>{
     private Context context;
-    private ArrayList<String> PL;
-
-    public void dataInit()
-    {
-        PL.add("C#");
-        PL.add("C++");
-        PL.add("Java");
-        PL.add("Kotlin");
-        PL.add("JavaScript");
-        PL.add("PHP 7.0");
-    }
+    private Vector<String> progLang;
+    private Vector<String> imageUrl;
 
     public ListAdapter(Context context)
     {
         context = this.context;
     }
 
+    public void dataInit()
+    {
+        PLanguage pLang = new PLanguage();
+        progLang = pLang.getName();
+        imageUrl = pLang.getImage();
+    }
+
     @NonNull
     @Override
     public ListAdapter.Handler onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(context).inflate(content_rview, viewGroup, false);
+        dataInit();
+        View view = LayoutInflater.from(context).inflate(R.layout.content_rview, viewGroup, false);
         Handler handler = new Handler(view);
         return handler;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ListAdapter.Handler handler, int i) {
+        handler.txt.setText(progLang.get(i));
 
+        Picasso.get().load(imageUrl.get(i))
+                .placeholder(R.mipmap.ic_launcher_round)
+                .error(R.mipmap.ic_launcher)
+                .into(handler.circleImg);
     }
 
     @Override
     public int getItemCount() {
-        PL.size();
+        return progLang.size();
     }
 
     public class Handler extends RecyclerView.ViewHolder{
-        public Handler(View views)
-        {
+        CircleImageView circleImg;
+        TextView txt;
 
+        public Handler(@NonNull View views)
+        {
+            super(views);
+            circleImg = views.findViewById(R.id.content_images);
+            txt = views.findViewById(R.id.proglang);
         }
     }
 }
--- app/src/main/java/com/example/javarview/model/PLanguage.java
+++ app/src/main/java/com/example/javarview/model/PLanguage.java
@@ -1,4 +1,41 @@
 package com.example.javarview.model;
 
+import java.util.Vector;
+
 public class PLanguage {
+    private Vector<String> name;
+    private Vector<String> image;
+
+    public Vector<String> getName() {
+        return name;
+    }
+
+    public void setName(Vector<String> name) {
+        this.name = name;
+    }
+
+    public Vector<String> getImage() {
+        return image;
+    }
+
+    public void setImage(Vector<String> image) {
+        this.image = image;
+    }
+
+    public PLanguage(){
+        name.add("C#");
+        name.add("C++");
+        name.add("Java");
+        name.add("Kotlin");
+
+        image.add("@drawable/CSharp");
+        image.add("@drawable/Cplusplus");
+        image.add("@drawable/java");
+        image.add("@drawable/kotlin");
+    }
 }
+
+
+
+
+
